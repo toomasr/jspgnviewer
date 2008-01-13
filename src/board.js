0c4f2307348198128c1c08561be280528807e4be
Toomas Römer <toomasr@gmail.com>
2008-01-13 10:34:17 +0000

    git-svn-id: http://jspgnviewer.googlecode.com/svn/trunk@141 fa0d4a12-9922-0410-b1fc-cf3abf164ca3

--- src/board.js
+++ src/board.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2006 Toomas Römer
+ * Copyright 2008 Toomas Römer
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
